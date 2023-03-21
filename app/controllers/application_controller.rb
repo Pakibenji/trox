990567d2136ae58a199db7e628124f8ed6827adc
JulienDast <julien.dast44@gmail.com>
2023-03-21 16:30:32 +0100

    Add image_url to User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    attributes = [:firstname, :lastname, :phone, :address, :address_complement, :postcode]
+    attributes = [:firstname, :lastname, :phone, :address, :address_complement, :postcode, :image_url]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
