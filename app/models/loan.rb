c9354c75da7184a474914567a636bcf79243d3d7
Alcade <alcade.benjamin@gmail.com>
2023-03-23 14:47:48 +0100

    add end_date method

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -3,5 +3,11 @@ class Loan < ApplicationRecord
     belongs_to :tool
     validates :start_date, presence: true
     validates :end_date, presence: true
-    
+    validate :end_date_after_start_date
+
+    def end_date_after_start_date
+        if start_date.present? && end_date.present? && end_date <= start_date
+          errors.add(:end_date, "must be after start date")
+        end
+      end
 end
