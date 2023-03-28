f2c9e00921c42bebeb611eff446992c068de43a6
Alcade <alcade.benjamin@gmail.com>
2023-03-28 10:55:17 +0200

    remove contactpage

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,6 @@ Rails.application.routes.draw do
   resources :users
 
   get 'about', to: 'static_pages#about'
-  get 'contact', to: 'static_pages#contact'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
