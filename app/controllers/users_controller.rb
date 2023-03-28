baa14d75b08650a63ed9adbda6c002144a05528b
Alcade <alcade.benjamin@gmail.com>
2023-03-28 09:38:49 +0200

    addusershowtools

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def show_tools
+    @user = current_user
+    @tools = @user.tools
+  end
 end
