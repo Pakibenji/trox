d9a5d041102a3b8855d07132a1fb7533bef6453b
Alcade <alcade.benjamin@gmail.com>
2023-03-30 06:59:47 +0200

    fixesmisc

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -8,6 +8,7 @@ class Loan < ApplicationRecord
     before_create :loaned_true_or_false
     after_destroy :mark_tool_as_false
     after_create :loan_send
+    validate :cant_loan_before_today
 
     def loan_send
       UserMailer.loan_email(self).deliver_now
@@ -24,15 +25,24 @@ class Loan < ApplicationRecord
       tool.update(loaned: true)
     end
 
+
+
     def end_date_after_start_date
         if start_date.present? && end_date.present? && end_date <= start_date
-          errors.add(:end_date, "must be after start date")
+          errors.add(:end_date, "La date de retour doit être postérieure à la date d'emprunt")
         end
       end
 
       def loaned_true_or_false
         if tool.loaned?
-          errors.add("cet objet est déjà loué")
+          errors.add("cet objet est déjà emprunté")
       end
     end
+
+    def cant_loan_before_today
+      if start_date.present? && start_date < Date.today
+        errors.add(:start_date, "Date d'emprunt invalide")
+      end
+    end
+
 end
