cec95272f733fce1d3b72da9b35ce740239f9920
Alcade <alcade.benjamin@gmail.com>
2023-03-30 14:22:48 +0200

    adduserinadmindashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 require 'faker'
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admintrox@yopmail.com', password: 'maxlamenace', password_confirmation: 'maxlamenace') if Rails.env.development?
