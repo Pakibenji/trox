8aadfdbf165069cf71814260c6651791ca6c30fb
Alcade <alcade.benjamin@gmail.com>
2023-03-31 12:02:54 +0200

    add admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@
 require 'faker'
 
 if Rails.env.development?
-  AdminUser.create!(email: 'admin@example.com', password: 'password',
-                    password_confirmation: 'password')
+  AdminUser.create!(email:ENV['ADMIN_MAIL'], password: ENV['ADMIN_PASSWORD'],
+    password_confirmation: ENV['ADMIN_PASSWORD'])
 end
